dc10ef6469dbc475d4ddb8f69bf94552618ed08c
Damian Krzeminski <pirxpilot@furkot.com>
2021-07-05 08:14:02 +0200

    use better-sqlite3

--- test/info.test.js
+++ test/info.test.js
@@ -1,42 +1,32 @@
-require('sqlite3').verbose();
+const tape = require('tape');
+const MBTiles = require('..');
 
-var fs = require('fs');
-var tape = require('tape');
-var MBTiles = require('..');
-var fixtures = {
-    plain_1: __dirname + '/fixtures/plain_1.mbtiles',
-    empty: __dirname + '/fixtures/empty.mbtiles'
+const fixtures = {
+  plain_1: `${__dirname}/fixtures/plain_1.mbtiles`,
 };
 
-try { fs.unlinkSync(fixtures.empty); } catch (err) {}
+tape('get metadata', function (assert) {
+  const mbtiles = new MBTiles(fixtures.plain_1);
+  const data = mbtiles.getInfo();
 
-tape('get metadata', function(assert) {
-    new MBTiles(fixtures.plain_1, function(err, mbtiles) {
-        assert.ifError(err);
+  assert.deepEqual({
+    name: 'plain_1',
+    description: 'demo description',
+    version: '1.0.3',
+    scheme: 'xyz',
+    minzoom: 0,
+    maxzoom: 4,
+    formatter: null,
+    center: [0, 7.500000001278025, 2],
+    bounds: [-179.9999999749438, -69.99999999526695, 179.9999999749438, 84.99999999782301],
+    // Test that json data is merged in.
+    level1: { level2: 'property' },
+    // These aren't part of TileJSON, but exist in an MBTiles file.
+    filesize: 561152,
+    type: 'baselayer',
+    id: 'plain_1',
+    basename: 'plain_1.mbtiles'
+  }, data);
 
-        mbtiles.getInfo(function(err, data) {
-            assert.ifError(err);
-
-            assert.deepEqual({
-                name: 'plain_1',
-                description: 'demo description',
-                version: '1.0.3',
-                scheme: 'xyz',
-                minzoom: 0,
-                maxzoom: 4,
-                formatter: null,
-                center: [ 0, 7.500000001278025, 2 ],
-                bounds: [ -179.9999999749438, -69.99999999526695, 179.9999999749438, 84.99999999782301 ],
-                // Test that json data is merged in.
-                level1: { level2: 'property' },
-                // These aren't part of TileJSON, but exist in an MBTiles file.
-                filesize: 561152,
-                type: 'baselayer',
-                id: 'plain_1',
-                basename: 'plain_1.mbtiles'
-            }, data);
-
-            assert.end();
-        });
-    });
+  assert.end();
 });
--- test/read.test.js
+++ test/read.test.js
@@ -1,93 +1,86 @@
-require('sqlite3').verbose();
+const fs = require('fs');
+const MBTiles = require('..');
+const tape = require('tape');
 
-var fs = require('fs');
-var MBTiles = require('..');
-var tape = require('tape');
-
-var fixtures = {
-    plain_1: __dirname + '/fixtures/plain_1.mbtiles',
-    plain_2: __dirname + '/fixtures/plain_2.mbtiles',
-    plain_3: __dirname + '/fixtures/plain_3.mbtiles',
-    plain_4: __dirname + '/fixtures/plain_4.mbtiles',
-    corrupt: __dirname + '/fixtures/corrupt.mbtiles',
-    corrupt_null_tile: __dirname + '/fixtures/corrupt_null_tile.mbtiles'
+const fixtures = {
+  plain_1: `${__dirname}/fixtures/plain_1.mbtiles`,
+  plain_2: `${__dirname}/fixtures/plain_2.mbtiles`,
+  plain_3: `${__dirname}/fixtures/plain_3.mbtiles`,
+  plain_4: `${__dirname}/fixtures/plain_4.mbtiles`,
+  corrupt: `${__dirname}/fixtures/corrupt.mbtiles`,
+  corrupt_null_tile: `${__dirname}/fixtures/corrupt_null_tile.mbtiles`
 };
 
-function yieldsError(assert, error, msg, callback) {
-    return function(err) {
-        assert.ok(err);
-        var re = new RegExp( "^" + msg, "i");
-        assert.ok(err.message.match(re));
-        if (callback) callback();
-    };
+function assertError(assert, err, msg) {
+  assert.ok(err, msg);
+  const re = new RegExp(`^${msg}`, "i");
+  assert.ok(err.message.match(re));
 }
 
-var loaded = {};
+const loaded = {};
 
-tape('setup', function(assert) {
-    var queue = Object.keys(fixtures);
-    var load = function() {
-        if (!queue.length) return assert.end();
-        var key = queue.shift();
-        new MBTiles(fixtures[key], function(err, mbtiles) {
-            if (err) throw err;
-            loaded[key] = mbtiles;
-            load();
-        });
-    };
-    load();
+tape('setup', assert => {
+  Object.keys(fixtures).forEach(key => {
+    loaded[key] = new MBTiles(fixtures[key]);
+  });
+  assert.end();
 });
 
-fs.readdirSync(__dirname + '/fixtures/images/').forEach(function(file) {
-    var coords = file.match(/^plain_1_(\d+)_(\d+)_(\d+).png$/);
-    if (!coords) return;
+fs.readdirSync(`${__dirname}/fixtures/images/`).forEach(file => {
+  let coords = file.match(/^plain_1_(\d+)_(\d+)_(\d+).png$/);
+  if (!coords) return;
 
-    // Flip Y coordinate because file names are TMS, but .getTile() expects XYZ.
-    coords = [ coords[3], coords[1], coords[2] ];
-    coords[2] = Math.pow(2, coords[0]) - 1 - coords[2];
-    tape('tile ' + coords.join('/'), function(assert) {
-        loaded.plain_1.getTile(coords[0] | 0, coords[1] | 0, coords[2] | 0, function(err, tile, headers) {
-            if (err) throw err;
-            assert.deepEqual(tile, fs.readFileSync(__dirname + '/fixtures/images/' + file));
-            assert.equal(headers['Content-Type'], 'image/png');
-            assert.ok(!isNaN(Date.parse(headers['Last-Modified'])));
-            assert.end();
-        });
-    });
+  // Flip Y coordinate because file names are TMS, but .getTile() expects XYZ.
+  coords = [coords[3], coords[1], coords[2]];
+  coords[2] = 2 ** coords[0] - 1 - coords[2];
+  tape(`tile ${coords.join('/')}`, assert => {
+    const { tile, headers, error } = loaded.plain_1.getTile(coords[0] | 0, coords[1] | 0, coords[2] | 0);
+    assert.ifError(error);
+    assert.deepEqual(tile, fs.readFileSync(`${__dirname}/fixtures/images/${file}`));
+    assert.equal(headers['Content-Type'], 'image/png');
+    assert.ok(!isNaN(Date.parse(headers['Last-Modified'])));
+    assert.end();
+  });
 });
-[   [0,1,0],
-    [-1,0,0],
-    [0,0,1],
-    [3,1,-1],
-    [2,-3,3],
-    [18,2,262140],
-    [4,0,15]
-].forEach(function(coords) {
-    tape('tile ' + coords.join('/'), function(assert) {
-        loaded.plain_1.getTile(coords[0], coords[1], coords[2], yieldsError(assert, 'error', 'Tile does not exist', assert.end));
-    });
-});
-
-[   [0,1,0],
-    [-1,0,0],
-    [0,0,-1],
-    [3,1,8],
-    [2,-3,0],
-    [18,2,3],
-    [4,0,0],
-    [4,3,8],
-    [4,4,8],
-    [4,5,8],
-    [4,13,4],
-    [4,0,14],
-    [3,0,7],
-    [3,6,2]
-].forEach(function(coords) {
-    tape('corrupt ' + coords.join('/'), function(assert) {
-        loaded.corrupt.getTile(coords[0], coords[1], coords[2], yieldsError(assert, 'error', 'SQLITE_CORRUPT: database disk image is malformed', assert.end));
-    });
+[[0, 1, 0],
+[-1, 0, 0],
+[0, 0, 1],
+[3, 1, -1],
+[2, -3, 3],
+[18, 2, 262140],
+[4, 0, 15]
+].forEach(coords => {
+  tape(`tile ${coords.join('/')}`, assert => {
+    const { error } = loaded.plain_1.getTile(coords[0], coords[1], coords[2]);
+    assertError(assert, error, 'Tile does not exist');
+    assert.end();
+  });
 });
 
-tape('corrupt null tile', function(assert) {
-    loaded.corrupt_null_tile.getTile(1, 0, 1, yieldsError(assert, 'error', 'Tile is invalid', assert.end));
+[[0, 1, 0],
+[-1, 0, 0],
+[0, 0, -1],
+[3, 1, 8],
+[2, -3, 0],
+[18, 2, 3],
+[4, 0, 0],
+[4, 3, 8],
+[4, 4, 8],
+[4, 5, 8],
+[4, 13, 4],
+[4, 0, 14],
+[3, 0, 7],
+[3, 6, 2]
+].forEach(coords => {
+  tape(`corrupt ${coords.join('/')}`, assert => {
+    const { error } = loaded.corrupt.getTile(coords[0], coords[1], coords[2]);
+    assertError(assert, error, 'database disk image is malformed');
+    assert.end();
+  });
+});
+
+tape('corrupt null tile', assert => {
+  const { error } = loaded.corrupt_null_tile.getTile(1, 0, 1);
+  assertError(assert, error, 'Tile is invalid');
+  assert.end();
 });

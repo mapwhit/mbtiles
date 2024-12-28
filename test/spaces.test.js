a7d530a719599a06da308ecd770a22c9bc371cc4
Damian Krzeminski <pirxpilot@furkot.com>
2024-12-28 09:02:14 -0700

    minor lint fixes

--- test/spaces.test.js
+++ test/spaces.test.js
@@ -3,7 +3,7 @@ const assert = require('node:assert/strict');
 const MBTiles = require('..');
 
 test('opens mbtiles file with spaces', function () {
-  const mbtiles = new MBTiles(__dirname + '/fixtures/with spaces.mbtiles')
+  const mbtiles = new MBTiles(__dirname + '/fixtures/with spaces.mbtiles');
   const info = mbtiles.getInfo();
   assert.deepEqual(info.level1, { level2: 'property' });
   assert.deepEqual(info.custom, ['custom list']);

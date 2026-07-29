85240845648df56a746448a61bb4a1acffe8aa80
Damian Krzeminski <pirxpilot@furkot.com>
2026-07-29 10:56:06 -0600

    upgrade `biome` to 2.5.6

--- test/spaces.test.js
+++ test/spaces.test.js
@@ -3,7 +3,7 @@ import MBTiles from '../lib/mbtiles.js';
 
 test('opens mbtiles file with spaces', t => {
   const mbtiles = new MBTiles(
-    import.meta.dirname + '/fixtures/with spaces.mbtiles'
+    `${import.meta.dirname}/fixtures/with spaces.mbtiles`
   );
   const info = mbtiles.getInfo();
   t.assert.deepEqual(info.level1, { level2: 'property' });
